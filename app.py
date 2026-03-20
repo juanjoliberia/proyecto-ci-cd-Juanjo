382973b31e39f375de2d8e7dc8c47038554bed94
juanjoliberia <Juanjo.liberia@gmail.com>
2026-03-20 16:29:52 +0000

    Paso9: Juanjo1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return ""¡Hola Juanjo! El pipeline de Jenkins"
+    return "¡Hola Juanjo! El pipeline de Jenkins"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
